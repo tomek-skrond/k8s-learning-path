7480b6328297dc7fb8a795f01857bc7ba14747c2
tomek-skrond <tomek2.skrond@gmail.com>
2023-06-02 11:40:42 +0000

    added setup script

---
+++ setup.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+WORKDIR=$(pwd)
+EDITOR=vim
+
+sudo ln -s /usr/local/bin/namespaces-docs ${WORKDIR}/docs/namespaces/namespaces.md
+sudo ln -s /usr/local/bin/helm-docs ${WORKDIR}/docs/helm/helm.md
+sudo ln -s /usr/local/bin/deployments-docs ${WORKDIR}/docs/deployments/deployments.md
+
+#docs aliases
+alias namespaces-docs="cat /usr/local/bin/namespaces-docs"
+alias helm-docs="cat /usr/local/bin/helm-docs"
+alias deployments-docs="cat /usr/local/bin/deployments-docs"
+
+alias namespaces-docs-edit="${EDITOR} /usr/local/bin/namespaces-docs"
+alias helm-docs-edit="${EDITOR} /usr/local/bin/helm-docs"
+alias deployments-docs-edit="${EDITOR} /usr/local/bin/deployments-docs"
+
+
